eab2803d96c62547394539e56668aeba7ea6abed
Aimee Barciauskas <aimee@developmentseed.org>
2023-01-21 15:34:07 -0800

    Working on refactoring

--- stac_ipyleaflet/core.py
+++ stac_ipyleaflet/core.py
@@ -1,12 +1,12 @@
 """Main module."""
 import csv
-import functools
+from io import BytesIO
 import json
 import os
 import re
 import requests
 
-from ipyleaflet import Map, DrawControl, WidgetControl, TileLayer
+from ipyleaflet import Map, DrawControl, WidgetControl, TileLayer, Popup
 from IPython.display import display
 import ipywidgets
 import matplotlib.pyplot as plt
@@ -14,6 +14,10 @@ import matplotlib.pyplot as plt
 from shapely.geometry import Polygon
 import rioxarray
 import xarray as xr
+import numpy
+from rio_tiler.io import Reader
+from rio_tiler.mosaic import mosaic_reader
+from rio_tiler.models import ImageData
 
 class StacIpyleaflet(Map):
     """
@@ -34,6 +38,7 @@ class StacIpyleaflet(Map):
 
         # Add rectangle draw control for bounding box
         # TODO(aimee): can we remove the other draw controls?
+        self.selected_data = []
         self.draw_control = None
         draw_control = DrawControl(
             edit=True,
@@ -42,7 +47,7 @@ class StacIpyleaflet(Map):
         draw_control.rectangle = {
             "shapeOptions": {
                 "fillColor": "transparent",
-                "color": "#fca45d",
+                "color": "#333",
                 "fillOpacity": 1.0
             }
         }
@@ -105,80 +110,103 @@ class StacIpyleaflet(Map):
         biomass_file = 'biomass-layers.csv'
         with open(biomass_file, newline='') as f:
             csv_reader = csv.reader(f)
+            next(csv_reader, None)  # skip the headers
             for row in csv_reader:
                 name, tile_url = row[0], row[1]
                 tile_layer = TileLayer(url=tile_url, attribution=name, name=name, visible=False)
                 self.add_layer(tile_layer)
 
-    # def create_histograms(self, b):
-    #     b = ipywidgets.Button(description='(50% width, 80px height) button',
-    #             layout=ipywidgets.Layout(width='50%', height='80px'))
-    #     hist_widget = ipywidgets.VBox()
-    #     hist_widget.children = [b]
-    #     histogram_control = WidgetControl(widget=hist_widget, position="topright")
-    #     self.add(histogram_control)
-    def create_histograms(self, b):
-        print("in create histograms")
-        minx, maxx = [0, 500]
-        plot_args = {"range": (minx, maxx)}
+    def gen_mosaic_dataset_reader(selv, assets, bounds):
+        # see https://github.com/cogeotiff/rio-tiler/blob/main/rio_tiler/io/rasterio.py#L368-L380
+        def _part_read(src_path: str, *args, **kwargs) -> ImageData:
+            with Reader(src_path) as src:
+                return src.part(bounds, *args, **kwargs)
+        # mosaic_reader will use multithreading to distribute the image fetching 
+        # and them merge all arrays together
+        img, _ = mosaic_reader(assets, reader=_part_read, max_size=512) # change the max_size to make it faster/slower 
+
+        data = img.as_masked()  # create Masked Array from ImageData
+        # Avoid non masked nan/inf values
+        numpy.ma.fix_invalid(data, copy=False)
+        return xr.DataArray(data)
+    
+    # TODO(aimee): compare performance, but right now this generates an error 
+    # ValueError: Could not find any dimension coordinates to use to order the datasets for concatenation
+    def gen_mosaic_dataset_crop(self, assets, str_bounds):
+        datasets = []
+        for asset in assets:
+            try:
+                crop_endpoint = f"{self.titiler_url}/cog/crop/{str_bounds}.npy?url={asset}&max_size=512"  # Same here you can either use max_size or width&height
+                res = requests.get(crop_endpoint)
+                arr = numpy.load(BytesIO(res.content))
+                tile, mask = arr[0:-1], arr[-1]
+
+                # TODO:
+                # convert tile/mask to xarray dataset
+                ds = xr.DataArray(tile)
+                datasets.append(ds)
+            except:
+                pass
+
+        ds = xr.combine_by_coords(datasets) #, fill_value=datasets[0]._FillValue)        
+
+    def update_selected_data(self):
         layers = self.layers
-        visible_layers = [layer for layer in layers if layer.visible and not layer.base]
-        fig = plt.figure()
+        visible_layers = [layer for layer in layers if type(layer) == TileLayer and layer.visible and not layer.base]
         geometries = [self.draw_control.last_draw['geometry']]
         if geometries[0]:
             box = Polygon(geometries[0]['coordinates'][0])
             bounds = box.bounds
             for idx, layer in enumerate(visible_layers):
                 layer_url = layer.url
-                title = layer.name.replace('_', ' ').upper()
-                axes = fig.add_subplot(int(f"22{idx+1}"))
-                match = re.search('url=(.+.tif)', layer_url)
-                plot_args['ax'] = axes
-                hist_widget = ipywidgets.VBox()
-                out = ipywidgets.Output()
-                # create a histogram from single COG
+                title = layer.name.replace('_', ' ').upper()  
+                match = re.search('url=(.+.tif)', layer_url) 
                 if match and match.group(1):
                     s3_url = match.group(1)
                     xds = rioxarray.open_rasterio(s3_url)
                     ds = xds.sel(x=slice(bounds[0], bounds[2]), y=slice(bounds[3], bounds[1]))
-                # create a histogram from mulitiple COGs via mosaicjson
                 else:
                     match = re.search('(https://.+)/tiles', layer_url)
                     if match:
                         mosaic_url = match.groups()[0]
                         str_bounds = f"{bounds[0]},{bounds[1]},{bounds[2]},{bounds[3]}"
                         assets_endpoint = f"{self.titiler_url}/mosaicjson/{str_bounds}/assets?url={mosaic_url}/mosaicjson"
-                        # create a dataset from multiple COGs?
+                        # create a dataset from multiple COGs
                         assets_response = requests.get(assets_endpoint)
                         datasets = []
-                        for asset in assets_response.json():
-                            # TODO: try/catch here in case of Access Denied
-                            xds = rioxarray.open_rasterio(asset)
-                            # TODO: some datasets have y in reverse, is that true for all?
-                            subset = xds.sel(x=slice(bounds[0], bounds[2]), y=slice(bounds[3], bounds[1]), band=1)
-                            if subset.size > 0:
-                                datasets.append(subset)
-                        ds = xr.combine_by_coords(datasets, fill_value=datasets[0]._FillValue)
-                    else:
-                        (f"no files for for {layer}")
+                        assets = assets_response.json()
+                        ds1 = self.gen_mosaic_dataset_reader(assets, bounds)
+                        #ds2 = self.gen_mosaic_dataset_crop(assets, str_bounds)
+                ds1.attrs["title"] = title
+                self.selected_data.append(ds1)
+        return self.selected_data
+
+    def create_histograms(self, b):
+        print("in create histograms")
+        minx, maxx = [0, 500]
+        plot_args = {"range": (minx, maxx)}
+        fig = plt.figure()
+        hist_widget = ipywidgets.VBox()
+        out = ipywidgets.Output()
+        out.clear_output()
+        selected_data = self.update_selected_data()
+        if len(selected_data) == 0:
+            with out:
+                print("No data selected")
+                display()
+                return
+        else:
+            for idx, dataset in enumerate(selected_data):
+                axes = fig.add_subplot(int(f"22{idx+1}"))
+                plot_args['ax'] = axes
+                # create a histogram
                 with out:
-                    if ds.any():
-                        ds.plot.hist(**plot_args)
-                        axes.set_title(title)
-                        display(fig)
-                    else:
-                        display(f"<p>no files for for {layer}</p>")
-            hist_widget.children = [out]
-            histogram_control = WidgetControl(widget=hist_widget, position="bottomright")
-            self.add(histogram_control)
-            # else:
-            #     hist_widget = ipywidgets.VBox()
-            #     out = ipywidgets.Output()
-            #     with out:
-            #         display(f"<p>No layers selected</p>")
-            #     hist_widget.children = [out]
-            #     histogram_control = WidgetControl(widget=hist_widget, position="bottomright")
-            #     self.add(histogram_control)
+                    dataset.plot.hist(**plot_args)
+                    axes.set_title(dataset.attrs['title'])
+                    display(fig)
+        hist_widget.children = [out]
+        histogram_layer = Popup(child=hist_widget, location=self.center, min_width=200, min_height=200)
+        self.add_layer(histogram_layer)
         return None
 
     def draw_biomass_map(self):
